6291b97e43c25304b79287792fd3f33c7120e216
Michael Schurter <michael@harvey.(none)>
2009-09-11 11:00:55 -0700

    Replace tabs with spaces (in vim: ":%retab")

--- hashtrack.js
+++ hashtrack.js
@@ -34,55 +34,55 @@ var hashtrack = {
     'interval': null,
 
     'check_hash': function() {
-	if (location.hash != hashtrack.last_hash)
-	{
-	    hashtrack.last_hash = location.hash;
-	    hashtrack.updateVars();
-	    hashtrack.call_onhashchange_callbacks();
-	}
+    if (location.hash != hashtrack.last_hash)
+    {
+        hashtrack.last_hash = location.hash;
+        hashtrack.updateVars();
+        hashtrack.call_onhashchange_callbacks();
+    }
     },
     'init': function() {
-	if (hashtrack.interval === null) {
-	    hashtrack.interval = setInterval(hashtrack.check_hash,
-					     hashtrack.frequency);
-	}
-	if (typeof hashtrack.vars == "undefined") {
-	    hashtrack.vars = {};
-	}
-	hashtrack.updateVars();
-	// Act on the hash as if it changed when the page loads, if its
-	// "important"
-	if (location.hash) {
-	    hashtrack.call_onhashchange_callbacks();
+    if (hashtrack.interval === null) {
+        hashtrack.interval = setInterval(hashtrack.check_hash,
+                         hashtrack.frequency);
+    }
+    if (typeof hashtrack.vars == "undefined") {
+        hashtrack.vars = {};
+    }
+    hashtrack.updateVars();
+    // Act on the hash as if it changed when the page loads, if its
+    // "important"
+    if (location.hash) {
+        hashtrack.call_onhashchange_callbacks();
         }
     },
     'setFrequency': function (freq) {
-	if (hashtrack.frequency != freq) {
-	    hashtrack.frequency = freq;
-	    if (hashtrack.interval) {
-		clearInterval(hashtrack.interval);
-		hashtrack.interval = setInterval(hashtrack.check_hash, hashtrack.frequency);
-	    }
-	}
+    if (hashtrack.frequency != freq) {
+        hashtrack.frequency = freq;
+        if (hashtrack.interval) {
+        clearInterval(hashtrack.interval);
+        hashtrack.interval = setInterval(hashtrack.check_hash, hashtrack.frequency);
+        }
+    }
     },
     'stop': function() {
-	clearInterval(hashtrack.interval);
-	hashtrack.interval = null;
+    clearInterval(hashtrack.interval);
+    hashtrack.interval = null;
     },
     'onhashchange': function(func, first_call) {
         hashtrack.onhashchange_callbacks.push(func);
-	if (first_call) {
-	    func(location.hash.slice(1));
-	}
+    if (first_call) {
+        func(location.hash.slice(1));
+    }
     },
     'onhashvarchange': function(varname, func, first_call) {
-	if (!(varname in hashtrack.onhashvarchange_callbacks)) {
-	    hashtrack.onhashvarchange_callbacks[varname] = [];
-	}
-	hashtrack.onhashvarchange_callbacks[varname].push(func);
-	if (first_call) {
-	    func(hashtrack.getVar(varname));
-	}
+    if (!(varname in hashtrack.onhashvarchange_callbacks)) {
+        hashtrack.onhashvarchange_callbacks[varname] = [];
+    }
+    hashtrack.onhashvarchange_callbacks[varname].push(func);
+    if (first_call) {
+        func(hashtrack.getVar(varname));
+    }
     },
     'call_onhashchange_callbacks': function() {
         var hash = window.location.hash.slice(1);
@@ -103,7 +103,7 @@ var hashtrack = {
         }
     },
     'updateVars': function () {
-	var vars = hashtrack.getAllVars();
+    var vars = hashtrack.getAllVars();
         for (var k in vars) {
             if (hashtrack.vars[k] != vars[k]) {
                 hashtrack.call_onhashvarchange_callbacks(k, vars[k]);
@@ -112,29 +112,29 @@ var hashtrack = {
         hashtrack.vars = vars;
     },
     'getAllVars': function () {
-	var hash = window.location.hash.slice(1, window.location.hash.length);
-	var vars = hash.split("&");
-	var result_vars = {};
-	for (var i=0;i<vars.length;i++) {
-	    var pair = vars[i].split("=");
-	    result_vars[pair[0]] = pair[1];
-	}
-	return result_vars;
+    var hash = window.location.hash.slice(1, window.location.hash.length);
+    var vars = hash.split("&");
+    var result_vars = {};
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        result_vars[pair[0]] = pair[1];
+    }
+    return result_vars;
     },
     'getVar': function (variable) {
-	return hashtrack.vars[variable];
+    return hashtrack.vars[variable];
     },
 
     'setVar': function (variable, value) {
-	var hash = window.location.hash.slice(1, window.location.hash.length);
-	var new_hash;
-	if (hash.indexOf(variable + '=') == -1) {
-	    new_hash = hash + '&' + variable + '=' + value;
-	} else {
-	    new_hash = hash.replace(variable + '=' + hashtrack.getVar(variable), variable + '=' + value);
-	}
-	window.location.hash = new_hash;
-	hashtrack.vars[variable] = value;
+    var hash = window.location.hash.slice(1, window.location.hash.length);
+    var new_hash;
+    if (hash.indexOf(variable + '=') == -1) {
+        new_hash = hash + '&' + variable + '=' + value;
+    } else {
+        new_hash = hash.replace(variable + '=' + hashtrack.getVar(variable), variable + '=' + value);
+    }
+    window.location.hash = new_hash;
+    hashtrack.vars[variable] = value;
     }
 };
 
